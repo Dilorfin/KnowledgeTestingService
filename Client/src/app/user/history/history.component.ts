7c6bc204c4947394f18fd60468b7f32dafdcb9da
Dilorfin <pavel.kuybida@gmail.com>
2020-05-14 14:01:52 +0300

    Add history page

--- Client/src/app/user/history/history.component.ts
+++ Client/src/app/user/history/history.component.ts
@@ -1,15 +1,21 @@
 import { Component, OnInit } from '@angular/core';
+import { TestResult } from 'src/app/_models/tests/test-result';
+import { Pagination } from 'src/app/_helpers/pagination';
+import { HistoryService } from './history.service';
 
 @Component({
-  selector: 'app-history',
-  templateUrl: './history.component.html',
-  styleUrls: ['./history.component.css']
+	selector: 'app-history',
+	templateUrl: './history.component.html',
+	styleUrls: ['./history.component.css']
 })
-export class HistoryComponent implements OnInit {
+export class HistoryComponent extends Pagination<TestResult> implements OnInit {
 
-  constructor() { }
+	constructor(private historyService: HistoryService) {
+		super((offset, count)=>this.historyService.getAllResults(offset, count));
+	}
 
-  ngOnInit(): void {
-  }
+	ngOnInit(): void {
+		this.openPage(0);
+	}
 
 }
---
+++ Client/src/app/user/history/history.service.ts
@@ -0,0 +1,21 @@
+import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
+import { TestResult } from 'src/app/_models/tests/test-result';
+import { HttpClient, HttpParams } from '@angular/common/http';
+import { Items } from 'src/app/_helpers/pagination';
+import { environment } from 'src/environments/environment';
+
+@Injectable({
+	providedIn: 'root'
+})
+export class HistoryService {
+
+	constructor(private http : HttpClient) { }
+
+	getAllResults(offset : number, count:number) : Observable<Items<TestResult>>{
+		const params = new HttpParams()
+			.set('offset', offset.toString())
+			.set('count', count.toString());
+		return this.http.get<Items<TestResult>>(`${environment.apiUrl}/TestResult/GetAllUserResults`, {params});
+	}
+}
