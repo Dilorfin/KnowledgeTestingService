3a619eef2ede7e74efdb079b533d7403ff25bae6
Dilorfin <pavel.kuybida@gmail.com>
2020-05-16 23:39:48 +0300

    Add test results dal

---
+++ Backend/KnowledgeTestingService.DAL/Repositories/TestResults/ITestResultRepository.cs
@@ -0,0 +1,46 @@
+﻿using KnowledgeTestingService.DAL.Entities;
+using System;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+
+namespace KnowledgeTestingService.DAL.Repositories.TestResults
+{
+    public interface ITestResultRepository : IDisposable
+    {
+        /// <summary>
+        /// Gets entity by id from data source.
+        /// </summary>
+        /// <param name="id">Entity id</param>
+        /// <returns>Returns entity by id</returns>
+        Task<TestResult> GetAsync(int id);
+        
+        /// <summary>
+        /// Adds entity to data source.
+        /// </summary>
+        /// <param name="entity">Entity to add</param>
+        void Add(TestResult entity);
+
+        /// <summary>
+        /// Get certain amount of user's test results starting from specified offset.
+        /// </summary>
+        /// <param name="userId">User's id whose results will be returned</param>
+        /// <param name="offset">Number of entities to skip</param>
+        /// <param name="count">Number of entities to return</param>
+        /// <returns>Returns count of entities from the offset</returns>
+        Task<IEnumerable<TestResult>> GetAllUsersTestResults(string userId, int offset, int count);
+
+        /// <summary>
+        /// Counts test results of certain user
+        /// </summary>
+        /// <param name="userId">Id of user whose results to count</param>
+        /// <returns>Returns number of user's test results</returns>
+        Task<long> LongCountAsync(string userId);
+        
+        /// <summary>
+        /// Get test results for range of tests
+        /// </summary>
+        /// <param name="testIds">Ids of tests</param>
+        /// <returns>Returns test results for tests</returns>
+        Task<IEnumerable<TestResult>> GetTestResultsForTestsRange(IEnumerable<int> testIds);
+    }
+}
---
+++ Backend/KnowledgeTestingService.DAL/Repositories/TestResults/TestResultRepository.cs
@@ -0,0 +1,56 @@
+﻿using KnowledgeTestingService.DAL.EF;
+using KnowledgeTestingService.DAL.Entities;
+using Microsoft.EntityFrameworkCore;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace KnowledgeTestingService.DAL.Repositories.TestResults
+{
+    public class TestResultRepository : Repository, ITestResultRepository
+    {
+        private readonly DbSet<TestResult> testResults;
+
+        public TestResultRepository(ApplicationDbContext dbContext)
+            : base(dbContext)
+        {
+            testResults = dbContext.TestResults;
+        }
+
+        public async Task<TestResult> GetAsync(int id)
+        {
+            return await testResults
+                .Include(tr => tr.Test)
+                .FirstOrDefaultAsync(tr => tr.Id == id);
+        }
+
+        public void Add(TestResult entity)
+        {
+            testResults.Add(entity);
+        }
+
+        public async Task<IEnumerable<TestResult>> GetAllUsersTestResults(string userId, int offset, int count)
+        {
+            return await testResults
+                .Where(tr => tr.UserId == userId)
+                .Skip(offset)
+                .Take(count)
+                .Include(tr => tr.Test)
+                .ToListAsync();
+        }
+
+        public async Task<long> LongCountAsync(string userId)
+        {
+            return await testResults
+                .Where(tr => tr.UserId == userId)
+                .LongCountAsync();
+        }
+
+        public async Task<IEnumerable<TestResult>> GetTestResultsForTestsRange(IEnumerable<int> testIds)
+        {
+            return await testResults
+                .Where(tr => testIds.Contains(tr.TestId))
+                .ToListAsync();
+        }
+    }
+}
