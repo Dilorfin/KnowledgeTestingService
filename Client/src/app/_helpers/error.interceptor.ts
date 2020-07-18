8a7667dd631b3a9138792b8d58067154920cbb82
Dilorfin <pavel.kuybida@gmail.com>
2020-07-18 17:02:38 +0300

    Add redirects to not found and forbidden pages on errors

--- Client/src/app/_helpers/error.interceptor.ts
+++ Client/src/app/_helpers/error.interceptor.ts
@@ -1,12 +1,13 @@
 import { Injectable } from '@angular/core';
 import { HttpRequest, HttpHandler, HttpEvent, HttpInterceptor } from '@angular/common/http';
+import { Router } from '@angular/router';
 import { Observable, throwError } from 'rxjs';
 import { catchError } from 'rxjs/operators';
 import { AuthService } from '../auth/auth.service';
 
 @Injectable()
 export class ErrorInterceptor implements HttpInterceptor {
-    constructor(private authService: AuthService) { }
+    constructor(private router: Router, private authService: AuthService) { }
 
     intercept(request: HttpRequest<any>, next: HttpHandler): Observable<HttpEvent<any>> {
         return next.handle(request).pipe(catchError(err => {
@@ -14,7 +15,14 @@ export class ErrorInterceptor implements HttpInterceptor {
                 // auto logout if 401 response returned from api
                 this.authService.logout();
             }
-
+            if (err.status === 403) {
+                // redirect to NotFound page
+                this.router.navigate(['/forbidden'])
+            }
+            if (err.status === 404) {
+                // redirect to NotFound page
+                this.router.navigate(['/notfound'])
+            }
             return throwError(err);
         }));
     }
